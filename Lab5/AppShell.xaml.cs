c9d34e9cce0b28cf1ac71ccb1695ad85aad4c10b
ValeriiaK06 <valeriya.kupriyan@gmail.com>
2026-04-20 14:15:01 +0300

    Добавьте файлы проекта.

---
+++ Lab5/AppShell.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Lab5.Views;
+using Lab5.Views.Details;
+
+namespace Lab5
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            // Реєстрація додаткових маршрутів (Вимоги 5, 9 та 10):
+
+            // Сторінка деталей (DetailPage1)
+            Routing.RegisterRoute("detailpage", typeof(DetailPage1));
+
+            // Службова сторінка довідки (HelpPage)
+            Routing.RegisterRoute("helppage", typeof(HelpPage));
+
+            // Сторінка для відображення динамічних даних (DynamicDataPage)
+            Routing.RegisterRoute("dynamicpage", typeof(DynamicDataPage));
+        }
+    }
+}
